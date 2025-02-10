9319285a56f7da77e848434e85ed25a67209530b
Bing Bhakdibhumi <barameeb@andrew.cmu.edu>
2025-02-10 00:40:06 -0500

    Converted indentation from spaces to tabs in answered.js

--- src/topics/answered.js
+++ src/topics/answered.js
@@ -5,53 +5,53 @@ const plugins = require('../plugins');
 
 
 module.exports = function (Topics) {
-    Topics.markAsAnswered = async function (tids, uid) {
-        if (!Array.isArray(tids) || !tids.length) {
-            return false;
-        }
+	Topics.markAsAnswered = async function (tids, uid) {
+		if (!Array.isArray(tids) || !tids.length) {
+			return false;
+		}
 
-        tids = _.uniq(tids).filter(tid => tid && utils.isNumber(tid));
+		tids = _.uniq(tids).filter(tid => tid && utils.isNumber(tid));
 
-        if (!tids.length) {
-            return false;
-        }
+		if (!tids.length) {
+			return false;
+		}
 
-        const topicData = await Topics.getTopicFields(tid, ['answered']);
-        if (!topicData || !topicData.cid) {
+		const topicData = await Topics.getTopicFields(tid, ['answered']);
+		if (!topicData || !topicData.cid) {
 			throw new Error('[[error:no-topic]]');
 		}
 
-        // function from ../data.js
-        await Topics.setTopicField(tid, 'answered', 1);
+		// function from ../data.js
+		await Topics.setTopicField(tid, 'answered', 1);
 		plugins.hooks.fire('action:topic.answered', { topic: _.clone(topicData), uid: uid });
 		return topicData;
-            
-    };
+			
+	};
 
-    Topics.markAsUnanswered = async function (uid, tid) {
-        if (!Array.isArray(tids) || !tids.length) {
-            return false;
-        }
+	Topics.markAsUnanswered = async function (uid, tid) {
+		if (!Array.isArray(tids) || !tids.length) {
+			return false;
+		}
 
-        tids = _.uniq(tids).filter(tid => tid && utils.isNumber(tid));
+		tids = _.uniq(tids).filter(tid => tid && utils.isNumber(tid));
 
-        if (!tids.length) {
-            return false;
-        }
+		if (!tids.length) {
+			return false;
+		}
 
-        const topicData = await Topics.getTopicFields(tid, ['answered']);
-        if (!topicData || !topicData.cid) {
+		const topicData = await Topics.getTopicFields(tid, ['answered']);
+		if (!topicData || !topicData.cid) {
 			throw new Error('[[error:no-topic]]');
 		}
 
-        await Topics.setTopicField(tid, 'answered', 0);
+		await Topics.setTopicField(tid, 'answered', 0);
 		plugins.hooks.fire('action:topic.answered', { topic: _.clone(topicData), uid: uid });
 		return topicData;
-            
-    };
+			
+	};
 
-    // based on getUnread functions from ../unread.js
-    Topics.getUnansweredTopics = async function (params) {
+	// based on getUnread functions from ../unread.js
+	Topics.getUnansweredTopics = async function (params) {
 		const unansweredTopics = {
 			showSelect: true,
 			nextStart: 0,
@@ -76,40 +76,40 @@ module.exports = function (Topics) {
 		return unansweredTopics;
 	};
 
-    Topics.getUnAnsweredTids = async function (params) {
-            const results = await Topics.getAnsweredData(params);
-            return params.count ? results.counts : results.tids;
-    };
-    
-    Topics.getAnsweredData = async function (params) {
-        const uid = parseInt(params.uid, 10);
+	Topics.getUnAnsweredTids = async function (params) {
+			const results = await Topics.getAnsweredData(params);
+			return params.count ? results.counts : results.tids;
+	};
+	
+	Topics.getAnsweredData = async function (params) {
+		const uid = parseInt(params.uid, 10);
 
-        params.filter = params.filter || '';
+		params.filter = params.filter || '';
 
-        if (params.cid && !Array.isArray(params.cid)) {
-            params.cid = [params.cid];
-        }
+		if (params.cid && !Array.isArray(params.cid)) {
+			params.cid = [params.cid];
+		}
 
-        if (params.tag && !Array.isArray(params.tag)) {
-            params.tag = [params.tag];
-        }
+		if (params.tag && !Array.isArray(params.tag)) {
+			params.tag = [params.tag];
+		}
 
-        const data = await getTids(params);
-        if (uid <= 0) {
-            return data;
-        }
+		const data = await getTids(params);
+		if (uid <= 0) {
+			return data;
+		}
 
-        const result = await plugins.hooks.fire('filter:topics.getAnsweredTids', {
-            uid: uid,
-            tids: data.tids,
-            counts: data.counts,
-            tidsByFilter: data.tidsByFilter,
-            answeredCids: data.answeredCids,
-            cid: params.cid,
-            filter: params.filter,
-            query: params.query || {},
-        });
+		const result = await plugins.hooks.fire('filter:topics.getAnsweredTids', {
+			uid: uid,
+			tids: data.tids,
+			counts: data.counts,
+			tidsByFilter: data.tidsByFilter,
+			answeredCids: data.answeredCids,
+			cid: params.cid,
+			filter: params.filter,
+			query: params.query || {},
+		});
 
-        return result;
-    };
+		return result;
+	};
 };
